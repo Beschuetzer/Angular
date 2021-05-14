ba26bb8fc8b1640b491015d2d4f2643bdeb1a7d5
Adam <adam.j.major@gmail.com>
2021-05-13 20:11:05 -0700

    Finish section 2

--- first-app/src/app/assignment-three/assignment-three.component.ts
+++ first-app/src/app/assignment-three/assignment-three.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment-three.component.css']
 })
 export class AssignmentThreeComponent implements OnInit {
+  shouldDisplayParagraph = true;
+  timeStamps: string[] = [];
+  assignmentName = 'Assignment Three';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onButtonClick(e: Event) {
+    this.timeStamps.push(new Date().toISOString());
+    this.shouldDisplayParagraph = !this.shouldDisplayParagraph;
+  }
+
+  getIsFifthPlusItem(timeStamp) {
+    console.log('timeStamp =', timeStamp);
+    return this.timeStamps.findIndex(ts => ts === timeStamp) >= 4;
   }
 
 }
