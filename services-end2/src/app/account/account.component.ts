897292ed974a99012b790bc5f4b389863ee72f49
Adam <adam.j.major@gmail.com>
2021-05-17 18:14:17 -0700

    Finish services module

--- services-end2/src/app/account/account.component.ts
+++ services-end2/src/app/account/account.component.ts
@@ -1,24 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountsService } from '../services/accounts.service';
-import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
   providers: [
-    LoggingService,
-    AccountsService,
   ],
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string} = {name: '', status: ''};
   @Input() id: number = -1;
 
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService){}
+  constructor(private accountsService: AccountsService){}
 
   onSetTo(status: string) {
-    this.loggingService.logStatusChange(status);
     this.accountsService.updateStatus(this.id, status);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
--- services-end2/src/app/new-account/new-account.component.ts
+++ services-end2/src/app/new-account/new-account.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 import { AccountsService } from '../services/accounts.service';
-import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
   providers: [
-    LoggingService,
-    AccountsService,
   ],
 })
 export class NewAccountComponent {
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {}
+  constructor(private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.loggingService.logStatusChange(accountStatus)
     this.accountsService.addAccount(accountName, accountStatus);
   }
 }
